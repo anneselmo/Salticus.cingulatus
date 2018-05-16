cf5a95c645775d3151989e244fc677fb73436511
Anneselmo <anothergnome@tuta.io>
2018-05-16 23:33:30 +0000

    generate get_list

--- cingulatus.py
+++ cingulatus.py
@@ -21,6 +21,27 @@ def merge_got_list(ngot_list, got_list):
              got_list[key]=ngot_list[key]
     return(got_list)
 
+def guess_times(llist):
+    glist={} 
+    for item in llist:
+        dif=0
+        while i > len(item[1]):
+            if i is not 0:
+                last=item[1][i]
+                dif=dif+(last - item[1][i-1])
+        avg=dif/(i+1)
+        glist[item]=int(last+avg)
+    return(glist)
+
+def gen_get_list(llist, time):
+    get_list=[]
+    ctime=int(datetime.datetime.now().timestamp())
+    glist=guess_times(llist)
+    for item in glist:
+        if ctime+time > glist[item]:
+            get_list.append(item)
+    return(get_list)
+                
 def crawl_loop():
     global got_list
     got_list={}
@@ -28,4 +49,5 @@ def crawl_loop():
     while true:
         got_list=merge_got_list(salt.countloop(get_list, rounds, limit, threads), got_list)
         get_list=gen_get_list(got_list)
+
 
