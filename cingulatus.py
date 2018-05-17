c7ab2a7b4764b781bdf1dedbf0326a9cec4bb64d
Anneselmo <anothergnome@tuta.io>
2018-05-17 18:53:28 +0000

    errors

--- cingulatus.py
+++ cingulatus.py
@@ -1,7 +1,8 @@
 #! /usr/bin/py
 
-import sleep
 import datetime
+
+from time import sleep
 
 import utils
 import salt
@@ -17,7 +18,7 @@ def merge_got_list(ngot_list, got_list):
             if got_list[key][0] is not ngot_list[key][0]:
                 got_list[key][0] = ngot_list[key][0]
                 got_list[key][1].append(ngot_list[key][1][0])
-         else:
+        else:
              got_list[key]=ngot_list[key]
     return(got_list)
 
@@ -44,10 +45,16 @@ def gen_get_list(llist, time):
                 
 def crawl_loop():
     global got_list
-    got_list={}
-
-    while true:
+    try:
+        got_list=cdb2dict(cpath+"/updates.dict.cdb")
+        get_list=gen_get_list(got_list, 86400)
+    except:
+        got_list={}
+    rounds=1
+    limit=320
+    threads=3
+    got_list=merge_got_list(salt.countloop(, rounds, limit, threads), got_list)
+    while True:
         got_list=merge_got_list(salt.countloop(get_list, rounds, limit, threads), got_list)
         get_list=gen_get_list(got_list)
-
-
+        utils.dict2cdb(got_list, path+"/updates.dict.cdb") 
